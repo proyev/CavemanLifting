1c7bf60c1a407a0bce8786a5d03d85bf607e9489
Aothe <[DLiamMorris96@gmail.com>
2021-11-10 17:09:37 +0000

    Tidy code & imports

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SessionList from './sessionlist/SessionList';
 
 export default function Dashboard() {
   return (
     <div>
       <p>Das board</p>
+      <SessionList />
     </div>
   );
 }
--- client/src/components/dashboard/sessionlist/SessionList.js
+++ client/src/components/dashboard/sessionlist/SessionList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Sessionlist() {
+export default function SessionList() {
   return (
     <div>
       <p>slist</p>

1e6ff82380b6e1f6207dce1467a38a61be5b1c20
Aothe <[DLiamMorris96@gmail.com>
2021-11-10 22:00:09 +0000

    gym.js displaying markers for each gym within a border box

--- client/src/components/gyms/Gym.js
+++ client/src/components/gyms/Gym.js
@@ -1,5 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
+import ApiService from '../../ApiService';
 import './Gym.css';
+
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
 export default function Gym() {
@@ -8,9 +10,29 @@ export default function Gym() {
 
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-6.26);
-  const [lat, setLat] = useState(53.348);
+  const [lng, setLng] = useState(-118);
+  const [lat, setLat] = useState(34);
   const [zoom, setZoom] = useState(15);
+
+  function addMarkers(data) {
+    /* For each feature in the GeoJSON object above: */
+    for (const marker of data.features) {
+      /* Create a div element for the marker. */
+      const el = document.createElement('div');
+      /* Assign a unique `id` to the marker. */
+      el.id = `marker-${marker.properties.id}`;
+      /* Assign the `marker` class to each marker for styling. */
+      el.className = 'marker';
+
+      /**
+       * Create a marker using the div element
+       * defined above and add it to the map.
+       **/
+      new mapboxgl.Marker(el, { offset: [0, -23] })
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map.current);
+    }
+  }
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -24,9 +46,26 @@ export default function Gym() {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/swarge/ckvrajarj98pc14pfqn5n4uwl',
+      style: 'mapbox://styles/swarge/ckvu2b0ju2l0z14ujuj06wvsh',
       center: [lng, lat],
       zoom: zoom,
+    });
+    map.current.on('load', async () => {
+      const gymLocations = await ApiService.getGeolocatedGyms(lat, lng);
+      console.log(gymLocations);
+      //   map.current.addLayer({
+      //     id: 'locations',
+      //     type: 'circle',
+      //     source: {
+      //       type: 'geojson',
+      //       data: gymLocations,
+      //     },
+      //   });
+      map.current.addSource('places', {
+        type: 'geojson',
+        data: gymLocations,
+      });
+      addMarkers(gymLocations);
     });
   });
 

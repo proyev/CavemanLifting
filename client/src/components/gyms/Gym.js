3a13bc9e0d79d67cb9193dab358cd043748fd24a
Aothe <[DLiamMorris96@gmail.com>
2021-11-11 09:00:28 +0000

    Gym locator now working with popups for locations with name and address

--- client/src/components/gyms/Gym.js
+++ client/src/components/gyms/Gym.js
@@ -4,6 +4,8 @@ import './Gym.css';
 
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
+// Pls don't hate me for how messy this is
+
 export default function Gym() {
   mapboxgl.accessToken =
     'pk.eyJ1Ijoic3dhcmdlIiwiYSI6ImNrdnJhZ3h0ejJhajgycW91NGh6a2RlanQifQ.aV2Rqje77LMUMfJI-McKEg';
@@ -17,10 +19,9 @@ export default function Gym() {
   function addMarkers(data) {
     /* For each feature in the GeoJSON object above: */
     for (const marker of data.features) {
-      /* Create a div element for the marker. */
       const el = document.createElement('div');
       /* Assign a unique `id` to the marker. */
-      el.id = `marker-${marker.properties.id}`;
+      el.id = `marker-${marker.id}`;
       /* Assign the `marker` class to each marker for styling. */
       el.className = 'marker';
 
@@ -31,7 +32,82 @@ export default function Gym() {
       new mapboxgl.Marker(el, { offset: [0, -23] })
         .setLngLat(marker.geometry.coordinates)
         .addTo(map.current);
+
+      el.addEventListener('click', (e) => {
+        /* Fly to the point */
+        flyToStore(marker);
+        /* Close all other popups and display popup for clicked store */
+        createPopUp(marker);
+        /* Highlight listing in sidebar */
+        const activeItem = document.getElementsByClassName('active');
+        e.stopPropagation();
+        if (activeItem[0]) {
+          activeItem[0].classList.remove('active');
+        }
+        const listing = document.getElementById(`listing-${marker.id}`);
+        // listing.classList.add('active');
+      });
     }
+  }
+
+  function buildLocationList(data) {
+    for (const gym of data.features) {
+      /* Add a new listing section to the sidebar. */
+      const listings = document.getElementById('listings');
+      const listing = listings.appendChild(document.createElement('div'));
+      /* Assign a unique `id` to the listing. */
+      listing.id = `listing-${gym.id}`;
+      /* Assign the `item` class to each listing for styling. */
+      listing.className = 'item';
+
+      /* Add the link to the individual listing created above. */
+      const link = listing.appendChild(document.createElement('a'));
+      link.href = '#';
+      link.className = 'title';
+      link.id = `link-${gym.id}`;
+      link.innerHTML = `${gym.text}`;
+
+      /* Add details to the individual listing. */
+      const details = listing.appendChild(document.createElement('div'));
+      details.innerHTML = `${gym.properties.address}`;
+      if (gym.properties.distance) {
+        const roundedDistance = Math.round(gym.properties.distance * 100) / 100;
+        details.innerHTML += `<div><strong>${roundedDistance} miles away</strong></div>`;
+      }
+      link.addEventListener('click', function () {
+        for (const feature of data.features) {
+          if (this.id === `link-${feature.id}`) {
+            flyToStore(feature);
+            createPopUp(feature);
+          }
+        }
+        const activeItem = document.getElementsByClassName('active');
+        if (activeItem[0]) {
+          activeItem[0].classList.remove('active');
+        }
+        this.parentNode.classList.add('active');
+      });
+    }
+  }
+
+  function flyToStore(currentFeature) {
+    map.current.flyTo({
+      center: currentFeature.geometry.coordinates,
+      zoom: 15,
+    });
+  }
+
+  function createPopUp(currentFeature) {
+    const popUps = document.getElementsByClassName('mapboxgl-popup');
+    /** Check if there is already a popup on the map and if so, remove it */
+    if (popUps[0]) popUps[0].remove();
+
+    const popup = new mapboxgl.Popup({ closeOnClick: false })
+      .setLngLat(currentFeature.geometry.coordinates)
+      .setHTML(
+        `<h3>${currentFeature.text}</h3><h4>${currentFeature.properties.address}</h4>`
+      )
+      .addTo(map.current);
   }
 
   useEffect(() => {
@@ -53,26 +129,25 @@ export default function Gym() {
     map.current.on('load', async () => {
       const gymLocations = await ApiService.getGeolocatedGyms(lat, lng);
       console.log(gymLocations);
-      //   map.current.addLayer({
-      //     id: 'locations',
-      //     type: 'circle',
-      //     source: {
-      //       type: 'geojson',
-      //       data: gymLocations,
-      //     },
-      //   });
+
       map.current.addSource('places', {
         type: 'geojson',
         data: gymLocations,
       });
       addMarkers(gymLocations);
+      buildLocationList(gymLocations);
     });
   });
 
   return (
     <div>
-      <p>gym</p>
-      <div ref={mapContainer} className="map-container" />
+      <div className="sidebar">
+        <div className="heading">
+          <h1>Local gyms</h1>
+        </div>
+        <div id="listings" className="listings"></div>
+      </div>
+      <div ref={mapContainer} className="map-container"></div>
     </div>
   );
 }

0dfe158866352f2238228d621623f96da9426625
Aothe <[DLiamMorris96@gmail.com>
2021-11-12 09:08:37 +0000

    Sidebar logic changed, routing taken to component above. <a> tags used in place of React <Link> tags as they are both essentially the same thing

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './Sidebar.css';
 
 import profilePic from '../../assets/caveman_profile_pic.PNG';
@@ -10,110 +9,204 @@ import dumbbell from '../../assets/fitness-centre.svg';
 import building from '../../assets/building.svg';
 import plus from '../../assets/plus.svg';
 
-export default function Sidebar({ toggleForm, componentDecider }) {
+export default function Sidebar({ toggleForm }) {
   return (
-    <Router>
-      <div className="sidebar__container">
-        <a href="/dashboard" className="text-center">
-          <img
-            alt="Navigate to dashboard"
-            src={logo}
-            width="200"
-            height="100"
-          ></img>
-        </a>
-        <hr />
-        <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item">
-            <a
-              href="/dashboard"
-              className="nav-link text-white"
-              aria-current="page"
-            >
-              <img
-                alt="Navigate to dashboard"
-                src={home}
-                className="me-2"
-                width="20"
-                height="20"
-              />
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/profile" className="nav-link text-white">
-              <img
-                alt="Navigate to profile"
-                src={user}
-                className="me-2"
-                width="20"
-                height="20"
-              />
-              Profile
-            </a>
-          </li>
-          <li>
-            <a href="/workoutinfo" className="nav-link text-white">
-              <img
-                alt="View workouts"
-                src={dumbbell}
-                className="me-2"
-                width="20"
-                height="20"
-              />
-              Workouts
-            </a>
-          </li>
-          <li>
-            <a href="/gyms" className="nav-link text-white">
-              <img
-                alt="View local gyms"
-                src={building}
-                className="me-2"
-                width="20"
-                height="20"
-              />
-              Gyms
-            </a>
-          </li>
-          <li>
-            <button className="nav-link text-white" onClick={toggleForm}>
-              <img
-                alt="Add a new session"
-                src={plus}
-                className="me-2"
-                width="20"
-                height="20"
-              />
-              Create New Session
-            </button>
-          </li>
-        </ul>
-        <hr />
-        <div className="dropdown">
+    <div className="sidebar__container">
+      <a href="/dashboard" className="text-center">
+        <img alt="Navigate to dashboard" src={logo} id="logo__pic"></img>
+      </a>
+      <hr />
+      <ul className="nav nav-pills flex-column mb-auto">
+        <li className="nav-item">
           <a
-            href="/profile"
-            className="d-flex align-items-center text-white text-decoration-none"
+            href="/dashboard"
+            className="nav-link text-white"
+            aria-current="page"
           >
             <img
-              src={profilePic}
-              alt=""
-              width="55"
-              height="55"
-              className="rounded-circle me-2"
+              alt="Navigate to dashboard"
+              src={home}
+              className="me-2"
+              width="20"
+              height="20"
             />
-            <strong>Unga Bunga</strong>
+            Home
           </a>
-          <Routes>
-            <Route path="/dashboard">{componentDecider('/dashboard')}</Route>
-            <Route path="/profile">{componentDecider('/profile')}</Route>
-            <Route path="/workoutinfo">
-              {componentDecider('/workoutinfo')}
-            </Route>
-            <Route path="/gyms">{componentDecider('/gyms')}</Route>
-          </Routes>
-        </div>
+        </li>
+        <li>
+          <a href="/profile" className="nav-link text-white">
+            <img
+              alt="Navigate to profile"
+              src={user}
+              className="me-2"
+              width="20"
+              height="20"
+            />
+            Profile
+          </a>
+        </li>
+        <li>
+          <a href="/workoutinfo" className="nav-link text-white">
+            <img
+              alt="View workouts"
+              src={dumbbell}
+              className="me-2"
+              width="20"
+              height="20"
+            />
+            Workouts
+          </a>
+        </li>
+        <li>
+          <a href="/gyms" className="nav-link text-white">
+            <img
+              alt="View local gyms"
+              src={building}
+              className="me-2"
+              width="20"
+              height="20"
+            />
+            Gyms
+          </a>
+        </li>
+        <li>
+          <button className="nav-link text-white" onClick={toggleForm}>
+            <img
+              alt="Add a new session"
+              src={plus}
+              className="me-2"
+              width="20"
+              height="20"
+            />
+            Create New Session
+          </button>
+        </li>
+      </ul>
+      <hr />
+      <div className="dropdown">
+        <a
+          href="/profile"
+          className="d-flex align-items-center text-white text-decoration-none"
+        >
+          <img
+            src={profilePic}
+            alt=""
+            width="55"
+            height="55"
+            className="rounded-circle me-2"
+          />
+          <strong>Unga Bunga</strong>
+        </a>
       </div>
-    </Router>
+    </div>
   );
 }
+
+// return (
+//   <Router>
+//     <div className="sidebar__container">
+//       <a href="/dashboard" className="text-center">
+//         <img
+//           alt="Navigate to dashboard"
+//           src={logo}
+//           // width="200"
+//           // height="100"
+//           id="logo__pic"
+//         ></img>
+//       </a>
+//       <hr />
+//       <ul className="nav nav-pills flex-column mb-auto">
+//         <li className="nav-item">
+//           <a
+//             href="/dashboard"
+//             className="nav-link text-white"
+//             aria-current="page"
+//           >
+//             <img
+//               alt="Navigate to dashboard"
+//               src={home}
+//               className="me-2"
+//               width="20"
+//               height="20"
+//             />
+//             Home
+//           </a>
+//         </li>
+//         <li>
+//           <a href="/profile" className="nav-link text-white">
+//             <img
+//               alt="Navigate to profile"
+//               src={user}
+//               className="me-2"
+//               width="20"
+//               height="20"
+//             />
+//             Profile
+//           </a>
+//         </li>
+//         <li>
+//           <a href="/workoutinfo" className="nav-link text-white">
+//             <img
+//               alt="View workouts"
+//               src={dumbbell}
+//               className="me-2"
+//               width="20"
+//               height="20"
+//             />
+//             Workouts
+//           </a>
+//         </li>
+//         <li>
+//           <a href="/gyms" className="nav-link text-white">
+//             <img
+//               alt="View local gyms"
+//               src={building}
+//               className="me-2"
+//               width="20"
+//               height="20"
+//             />
+//             Gyms
+//           </a>
+//         </li>
+//         <li>
+//           <button className="nav-link text-white" onClick={toggleForm}>
+//             <img
+//               alt="Add a new session"
+//               src={plus}
+//               className="me-2"
+//               width="20"
+//               height="20"
+//             />
+//             Create New Session
+//           </button>
+//         </li>
+//       </ul>
+//       <hr />
+//       <div className="dropdown">
+//         <a
+//           href="/profile"
+//           className="d-flex align-items-center text-white text-decoration-none"
+//         >
+//           <img
+//             src={profilePic}
+//             alt=""
+//             width="55"
+//             height="55"
+//             className="rounded-circle me-2"
+//           />
+//           <strong>Unga Bunga</strong>
+//         </a>
+//         <Routes>
+//           <Route path="/dashboard" element={componentDecider('/dashboard')} />
+//           <Route path="/profile" element={componentDecider('/profile')} />
+//           <Route
+//             path="/workoutinfo"
+//             element={componentDecider('/workoutinfo')}
+//           />
+//           <Route path="/gyms" element={componentDecider('/gyms')} />
+//         </Routes>
+//       </div>
+//     </div>
+//   </Router>
+// );
+// }

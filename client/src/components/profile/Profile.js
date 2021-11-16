070d162cdc9aae7c5e26fe52c20ae5d80d9bc755
Aothe <[DLiamMorris96@gmail.com>
2021-11-16 01:01:39 +0000

    Adding 2 boxes to profile as placeholders for Personal records & Charts

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import {
   Flex,
   // HStack,
   VStack,
+  Box,
   Heading,
   Text,
   Image,
@@ -40,86 +41,113 @@ export default function Profile({ workouts, userData }) {
       justifyContent="space-around"
     >
       <Flex
-        w="50rem"
-        p="2.5rem"
-        bg={tableBGColor}
-        align="center"
+        m={0}
+        direction="row"
         justify="space-evenly"
-        borderRadius="25px"
+        align="center"
+        w="100%"
       >
-        <Image
-          // alignSelf="flex-start"
-          // mr="10rem"
-          h="10rem"
-          borderRadius="10px"
-          src={profilePic}
-        />
-
-        <VStack
-          w="30rem"
-          m="1rem"
-          p="1rem"
-          align="flex-start"
-          justify="center"
-          borderColor="gray.700"
-        >
-          <Heading size="sm" mb="0rem">
-            Username:
-          </Heading>
-          {/* <Divider /> */}
-          <Text mb="0.25rem" fontWeight="semi-bold">
-            {userData.username}
-          </Text>
-          {/* <Heading size="sm">Personal Records:</Heading> */}
-          <Divider />
-          <Heading size="sm" mb="0rem">
-            Bio:
-          </Heading>
-
-          <Text size="lg" mb="0rem">
-            {userData.bio}
-          </Text>
-        </VStack>
-      </Flex>
-      {latest ? (
-        <Table
-          size="md"
-          variant="simple"
-          colorScheme="teal"
+        <Flex
+          w="40rem"
+          p="2.5rem"
           bg={tableBGColor}
+          align="center"
+          justify="space-evenly"
           borderRadius="25px"
-          w="75%"
         >
-          <TableCaption placement="top">Latest workout</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Lift</Th>
-              <Th>Weight</Th>
-              <Th>Sets</Th>
-              <Th>Reps</Th>
-              <Th>Rest</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr></Tr>
-            {latest.routine &&
-              latest.routine.map((routine) => {
-                console.log(routine);
-                return (
-                  <Tr size="lg" key={routine._id}>
-                    <Td>{routine.lift}</Td>
-                    <Td>{routine.weight}</Td>
-                    <Td>{routine.sets}</Td>
-                    <Td>{routine.reps}</Td>
-                    <Td>{routine.rest}</Td>
-                  </Tr>
-                );
-              })}
-          </Tbody>
-        </Table>
-      ) : (
-        <Text>Profile Loading</Text>
-      )}
+          <Image h="10rem" borderRadius="10px" src={profilePic} />
+
+          <VStack
+            w="30rem"
+            m="1rem"
+            p="1rem"
+            align="flex-start"
+            justify="center"
+            borderColor="gray.700"
+          >
+            <Heading size="sm" mb="0rem">
+              Username:
+            </Heading>
+            <Text mb="0.25rem" fontWeight="semi-bold">
+              {userData.username}
+            </Text>
+            <Divider />
+            <Heading size="sm" mb="0rem">
+              Bio:
+            </Heading>
+
+            <Text size="lg" mb="0rem">
+              {userData.bio}
+            </Text>
+          </VStack>
+        </Flex>
+        <Flex
+          h="100%"
+          w="30rem"
+          borderRadius="25px"
+          direction="column"
+          align="center"
+          justify="space-evenly"
+          bg={tableBGColor}
+        >
+          <Box>PRS</Box>
+
+          <Text>data</Text>
+        </Flex>
+      </Flex>
+      <Flex m={0} w="100%" justify="space-evenly" align="flex-end">
+        {latest ? (
+          <Table
+            size="lg"
+            variant="simple"
+            colorScheme="teal"
+            bg={tableBGColor}
+            borderRadius="25px"
+            w="40rem"
+          >
+            <TableCaption placement="top">Latest workout</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Lift</Th>
+                <Th>Weight</Th>
+                <Th>Sets</Th>
+                <Th>Reps</Th>
+                <Th>Rest</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr></Tr>
+              {latest.routine &&
+                latest.routine.map((routine) => {
+                  return (
+                    <Tr size="lg" key={routine._id}>
+                      <Td>{routine.lift}</Td>
+                      <Td>{routine.weight}</Td>
+                      <Td>{routine.sets}</Td>
+                      <Td>{routine.reps}</Td>
+                      <Td>{routine.rest}</Td>
+                    </Tr>
+                  );
+                })}
+            </Tbody>
+          </Table>
+        ) : (
+          <Text>Profile Loading</Text>
+        )}
+        <Flex
+          h="82.5%"
+          w="30rem"
+          borderRadius="25px"
+          direction="column"
+          align="center"
+          justify="space-evenly"
+          bg={tableBGColor}
+        >
+          <Box>Ahhh</Box>
+
+          <Text>Im charting</Text>
+        </Flex>
+      </Flex>
       {workouts.length && <Heatmap workouts={workouts} />}
     </Flex>
   );

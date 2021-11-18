ce95bfe056553a60532d06711237c8dbe7a3688e
proyev <prots.yevhen@gmail.com>
2021-11-18 19:31:40 +0100

    Start tsing

---
+++ server/server.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+
+import router as router express.Router from './router';
+
+// const db = require('./models');
+
+const PORT = 3001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+(async () => {
+  try {
+    // await db;
+    // console.log(`
+    //   DATABASE ${db}
+    // `);
+    app.listen(PORT, () => {
+      // console.log(`http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    // console.log(err);
+  }
+})();
+
+/*
+This is a relatively standard Express, MongoDB, Mongoose setup
+Checkout the controllers for additional comments on the code.
+*/
